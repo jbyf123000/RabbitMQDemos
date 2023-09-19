5879263e4bcdc9b6ac7ff6d35e4f40a82a9e9dad
jbyf123000 <jbyf123000@gmail.com>
2023-09-19 10:51:13 +0800

    设置队列持久化

--- WorkeQueue/src/main/java/top/jbyf/Publisher01.java
+++ WorkeQueue/src/main/java/top/jbyf/Publisher01.java
@@ -6,11 +6,11 @@ import top.jbyf.config.RabbitMqUtils;
 import java.util.Scanner;
 
 public class Publisher01 {
-    public static final String QUEUE_NAME="task";
+    public static final String QUEUE_NAME="task2";
 
     public static void main(String[] args) throws Exception {
         Channel channel = RabbitMqUtils.getChannel();
-        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        channel.queueDeclare(QUEUE_NAME,true,false,false,null);
         Scanner scanner = new Scanner(System.in);
         System.out.println("输入需要发送的消息：");
         while (scanner.hasNext()){
