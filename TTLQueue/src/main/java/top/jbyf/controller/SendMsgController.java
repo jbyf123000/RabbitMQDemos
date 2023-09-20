42243017141b5af5dc43a65add13b3e81b1079e0
jbyf123000 <jbyf123000@gmail.com>
2023-09-20 09:21:27 +0800

    延迟队列插件 生产者代码

--- TTLQueue/src/main/java/top/jbyf/controller/SendMsgController.java
+++ TTLQueue/src/main/java/top/jbyf/controller/SendMsgController.java
@@ -1,11 +1,13 @@
 package top.jbyf.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.jbyf.config.DelayQueueConfig;
 
 import javax.annotation.Resource;
 import java.util.Date;
@@ -32,6 +34,18 @@ public class SendMsgController {
         rabbitTemplate.convertAndSend("X","XC",message,(message1 -> {
             // 设置发送消息时候的延迟时长
             message1.getMessageProperties().setExpiration(ttlTime);
+            return message1;
+        }));
+    }
+
+    @ApiOperation("基于插件的消息和时间")
+    @GetMapping("sendDelayMsg/{message}/{delayTime}")
+    public void sendDelayMsg(@PathVariable String message,
+                             @PathVariable Integer delayTime){
+        log.info("当前时间：{} , 发送时长为{}毫秒的信息给延迟队列delayed.queue：{}",new Date().toString(),ttlTime,message);
+        rabbitTemplate.convertAndSend(DelayQueueConfig.DELAY_EXCHANGE_NAME,DelayQueueConfig.DELAY_ROUTING_KEY,message,(message1 -> {
+            // 设置 发送消息时候的 延迟时长 单位：ms
+            message1.getMessageProperties().setDelay(delayTime);
             return message1;
         }));
     }
