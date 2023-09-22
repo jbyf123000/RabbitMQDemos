65e9a2505b0070c7e54d6d80e6ec8fb4cf2fe70f
jbyf123000 <jbyf123000@gmail.com>
2023-09-22 12:03:02 +0800

    发布确认高级 配置类

---
+++ TTLQueue/src/main/java/top/jbyf/config/ConfirmConfig.java
@@ -0,0 +1,37 @@
+package top.jbyf.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Configuration
+public class ConfirmConfig {
+
+    // 交换机
+    public static final String CONFIRM_EXCHANGE_NAME = "confirm_exchange";
+    // 队列
+    public static final String CONFIRM_QUEUE_NAME = "confirm_queue";
+    // routing key
+    public static final String CONFIRM_ROUTING_KEY = "key1";
+
+    @Bean
+    public DirectExchange confirmExchange(){
+        return new DirectExchange(CONFIRM_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue confirmQueue(){
+        return QueueBuilder.durable(CONFIRM_QUEUE_NAME).build();
+    }
+
+    @Bean
+    public Binding confirmQueueBindingExchange(@Qualifier("confirmQueue") Queue confirmQueue,
+                                               @Qualifier("confirmExchange") DirectExchange confirmExchange){
+        return BindingBuilder
+                .bind(confirmQueue)
+                .to(confirmExchange)
+                .with(CONFIRM_ROUTING_KEY);
+    }
+
+}
